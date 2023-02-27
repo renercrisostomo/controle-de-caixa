b24d1477bfbe47ac0de5840cfd7a11a5d708f00c
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2023-02-27 13:39:48 -0300

    chamando os componentes em appjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Form, Graph, Tabela } from './shared/components';
+import theme from './shared/theme/theme';
+import { ThemeProvider } from '@mui/material';
 
 function App() {
+
+  const [tableData, setTableData] = useState([])
+  const [graphData, setgraphData] = useState({receita: 0, despesa: 0})
+
+  useEffect(() => {
+    let receitasTotal = 0;
+    let despesasTotal = 0;
+
+    tableData.forEach((item) => {
+      if (item.type === "Receita") {
+        receitasTotal += parseFloat(item.value);
+      } else if (item.type === "Despesa") {
+        despesasTotal += parseFloat(item.value);
+      }
+    });
+
+    setgraphData({receita: receitasTotal, despesa: despesasTotal});
+  }, [tableData]);
+
+
+  function onAddItem(newItem) {
+    const newTableData = [...tableData, newItem]
+    setTableData(newTableData)
+  }
+
+  function handleDelete(id) {
+    const newTableData = tableData.filter((item) => item.id !== id)
+    setTableData(newTableData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <ThemeProvider theme={theme}>
+        <h1>CONTROLE DE CAIXA</h1>
+        <div id='sections'>
+          <Form onAddItem={onAddItem} />
+          <Graph graphData={graphData} />
+          <Tabela tableData={tableData} handleDelete={handleDelete} />
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
